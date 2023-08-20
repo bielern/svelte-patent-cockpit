d60f56c4331c08501c1e515809be09d1130ff77e
Noah Bieler <noah.bieler@gmail.com>
2023-08-20 16:22:58 +0200

    Progressive enhancement on login form for errors

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,7 +1,14 @@
+<script lang="ts">
+  import {enhance} from '$app/forms';
+  import  type { ActionData } from './$types';
+
+  export let form: ActionData
+
+</script>
 
 <h1>Login</h1>
 
-<form class="flex flex-col gap-4" method="POST">
+<form class="flex flex-col gap-4" method="POST" use:enhance>
   <label class="block w-full">
     <div class="text-slate-600">Email</div>
     <input class="border border-blue-200 rounded py-2 px-2 w-full" name="email" type="email">
@@ -12,4 +19,9 @@
   </label>
   <button class="mt-4 px-3 py-2 bg-blue-500 hover:bg-blue-400 text-white">Log in</button>
 </form>
+
+{#if form?.error}
+<div class="mt-4 text-red-500">{form.error}</div>
+{/if}
+
 <a href="/signup" class="block mt-4 text-gray-500 underline hover:text-blue-500">No login? Sign up!</a>
